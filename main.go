3c6f2007cac05270a7bc6cd48f08f9ce90b28981
John Ranzie <79166716+jaranzie@users.noreply.github.com>
2022-10-02 02:57:01 -0400

    Add files via upload

--- main.go
+++ main.go
@@ -2,25 +2,31 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/session"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"math/rand"
+	"net/url"
+	"os/exec"
 	"time"
 )
 
-var mongoURL string = ""
+/* hack for TLS */
+
+/**/
+
+var gameIndex int32 = 1
 
 type RegisteredUser struct {
-	Username   string               `bson:"username" json:"username"`
-	Email      string               `bson:"email" json:"email"`
-	Password   string               `bson:"password" json:"password"`
-	Games      []primitive.ObjectID `bson:"games"`
-	RecentGame Game                 `RecentGame:"recentgame"`
+	Username   string  `bson:"username" json:"username"`
+	Email      string  `bson:"email" json:"email"`
+	Password   string  `bson:"password" json:"password"`
+	Games      []int32 `bson:"games"`
+	RecentGame int32   `bson:"recentgame"`
 }
 
 type UnregisteredUser struct {
@@ -31,12 +37,25 @@ type UnregisteredUser struct {
 }
 
 type Game struct {
-	Grid   []rune `bson:"grid" json:"grid"`
-	Winner rune   `bson:"winner" json:"winner"`
+	Id        int32  `bson:"_id" json:"id"`
+	Grid      []rune `bson:"grid" json:"grid"`
+	Winner    rune   `bson:"winner" json:"winner"`
+	StartDate string `bson:"start_date" json:"start_date"`
 }
 
 type Move struct {
 	Move int `json:"move"`
+}
+
+func sendEmail(user *UnregisteredUser) error {
+	//msg := []byte(fmt.Sprintf("jaranzie.cse356.compas.cs.stonybrook.edu/verify?email=%s&key=%s", user.Email, user.VerificationCode)) -a "FROM:test@group1.cse356.compas.cs.stonybrook.edu"
+	cmd := fmt.Sprintf("echo \"http://group1.cse356.compas.cs.stonybrook.edu/verify?email=%s&key=%s\" | mail --content-type 'text/plain; charset=ascii' -s \"Verify\" --encoding=quoted-printable %s", url.QueryEscape(user.Email), url.QueryEscape(user.VerificationCode), user.Email)
+	//("127.0.0.1:25", "test@jaranzie.cse356.compas.cs.stonybrook.edu", []string{user.Email}, msg)
+	_, err := exec.Command("bash", "-c", cmd).Output()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func randomString(length int) string {
@@ -44,13 +63,6 @@ func randomString(length int) string {
 	b := make([]byte, length)
 	rand.Read(b)
 	return fmt.Sprintf("%x", b)[:length]
-}
-
-func newGame() Game {
-	var g Game
-	g.Grid = []rune{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
-	g.Winner = ' '
-	return g
 }
 
 func playGame(g *Game) {
@@ -114,6 +126,22 @@ func checkWinner(g *Game) rune {
 	return ' '
 }
 
+/* Returns Inserted ID */
+func newGame(gameCollection *mongo.Collection) int32 {
+	var g Game
+	g.Grid = []rune{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
+	g.Winner = ' '
+	g.StartDate = time.Now().String()
+	g.Id = gameIndex
+	gameIndex++
+	one, err := gameCollection.InsertOne(context.TODO(), g)
+	if err != nil {
+		fmt.Println("Not inserted")
+		return -1
+	}
+	return one.InsertedID.(int32)
+}
+
 func main() {
 	/*
 		Connecting to Mongo
@@ -129,7 +157,6 @@ func main() {
 	registeredusers := client.Database("warmup2").Collection("RegisteredUser")
 	unregisteredusers := client.Database("warmup2").Collection("UnregisteredUser")
 	games := client.Database("warmup2").Collection("Games")
-	//activeGames := client.Database("warmup2").Collection("ActiveGames")
 
 	app := fiber.New()
 	store := session.New()
@@ -145,39 +172,69 @@ func main() {
 		if err := c.BodyParser(&newUser); err != nil {
 			return err
 		}
+		res := unregisteredusers.FindOne(context.TODO(), bson.D{{`email`, newUser.Email}})
+		if res.Err() != mongo.ErrNoDocuments {
+			return c.JSON(fiber.Map{"status": "ERROR"})
+		}
+		res = unregisteredusers.FindOne(context.TODO(), bson.D{{`username`, newUser.Username}})
+		if res.Err() != mongo.ErrNoDocuments {
+			return c.JSON(fiber.Map{"status": "ERROR"})
+		}
+		res = registeredusers.FindOne(context.TODO(), bson.D{{`username`, newUser.Username}})
+		if res.Err() != mongo.ErrNoDocuments {
+			return c.JSON(fiber.Map{"status": "ERROR"})
+		}
+		res = registeredusers.FindOne(context.TODO(), bson.D{{`email`, newUser.Email}})
+		if res.Err() != mongo.ErrNoDocuments {
+			return c.JSON(fiber.Map{"status": "ERROR"})
+		}
 		if len(newUser.Username) == 0 || len(newUser.Email) == 0 || len(newUser.Password) == 0 {
-			return err
+			return c.JSON(fiber.Map{"status": "ERROR"})
 		}
 		newUser.VerificationCode = randomString(10)
-		go func() {
-			//SEND MAIL "url/verify?email=newUser.email&key=newUser.verificationCode
-			_, err = unregisteredusers.InsertOne(context.TODO(), newUser)
-		}()
+		err := sendEmail(&newUser)
+		if err != nil {
+			return c.JSON(fiber.Map{"status": "ERROR"})
+		}
+		_, err = unregisteredusers.InsertOne(context.TODO(), newUser)
 		//Check errors
-		return c.SendString("User Creation Success")
+		return c.JSON(fiber.Map{"status": "OK"})
 	})
 
+	/*app.Get("/verify", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{"status": "OK"})
+	})*/
+
 	app.Get("/verify", func(c *fiber.Ctx) error {
-		email := c.Query("email")
-		key := c.Query("key")
+		email, _ := url.PathUnescape(c.Query("email"))
+		key, _ := url.PathUnescape(c.Query("key"))
 		var u UnregisteredUser
-		err := unregisteredusers.FindOne(context.TODO(), bson.D{{`email`, email}}).Decode(&u)
+		res := unregisteredusers.FindOne(context.TODO(), bson.D{{`email`, email}})
+		if res.Err() == mongo.ErrNoDocuments {
+			return c.JSON(fiber.Map{"status": "ERROR"})
+		}
+		err := res.Decode(&u)
 		if err != nil {
-			return fiber.ErrBadRequest
+			return c.JSON(fiber.Map{"status": "ERROR"})
 		}
 		if u.VerificationCode == key {
-			r := RegisteredUser{u.Username, u.Email, u.Password, []primitive.ObjectID{}, newGame()}
-			_, err = registeredusers.InsertOne(context.TODO(), r)
-			if err != nil {
-				return fiber.ErrBadRequest
+			newGameID := newGame(games)
+			if newGameID < 0 {
+				fmt.Println("Problem inserting")
+				return c.JSON(fiber.Map{"status": "ERROR"})
 			}
-			_, err = unregisteredusers.DeleteOne(context.TODO(), bson.D{{`email`, email}})
+			r := RegisteredUser{u.Username, u.Email, u.Password, []int32{}, newGameID}
+			_, err := registeredusers.InsertOne(context.TODO(), r)
 			if err != nil {
-				return fiber.ErrBadRequest
+				return c.JSON(fiber.Map{"status": "ERROR"})
 			}
-			return c.SendString("Verified")
+			_, err = unregisteredusers.DeleteOne(context.TODO(), bson.M{"email": email})
+			if err != nil {
+				return c.JSON(fiber.Map{"status": "ERROR"})
+			}
+			return c.JSON(fiber.Map{"status": "OK"})
 		}
-		return fiber.ErrBadRequest
+		return c.JSON(fiber.Map{"status": "ERROR"})
 	})
 
 	app.Post("/login", func(c *fiber.Ctx) error {
@@ -186,11 +243,15 @@ func main() {
 		_ = c.BodyParser(&user)
 		err := registeredusers.FindOne(context.TODO(), bson.D{{`username`, user.Username}}).Decode(&authUser)
 		if err != nil || user.Password != authUser.Password {
-			return fiber.ErrBadRequest
+			return c.JSON(fiber.Map{"status": "ERROR"})
 		}
 		sess, _ := store.Get(c)
 		sess.Set("username", authUser.Username)
-		return c.SendString("Login Succesful")
+		err = sess.Save()
+		if err != nil {
+			return c.JSON(fiber.Map{"status": "ERROR"})
+		}
+		return c.JSON(fiber.Map{"status": "OK"})
 	})
 
 	app.Post("/logout", func(c *fiber.Ctx) error {
@@ -199,30 +260,139 @@ func main() {
 			return fiber.ErrBadRequest
 		}
 		_ = sess.Destroy()
-		return c.SendString("Logout Succesful")
+		err = sess.Save()
+		if err != nil {
+			return c.JSON(fiber.Map{"status": "ERROR"})
+		}
+		return c.JSON(fiber.Map{"status": "OK"})
 	})
 
 	app.Post("/ttt/play", func(c *fiber.Ctx) error {
 		var mo Move
 		if err := c.BodyParser(mo); err != nil {
-			return err
+			return c.JSON(fiber.Map{"status": "ERROR"})
 		}
 		sess, err := store.Get(c)
 		if err != nil {
-			return c.SendString("Not logged in")
+			return c.JSON(fiber.Map{"status": "ERROR"})
 		}
 		var user RegisteredUser
 		err = registeredusers.FindOne(context.TODO(), bson.D{{`username`, sess.Get("username")}}).Decode(&user)
-		grid := user.RecentGame.Grid
+		var game Game
+		err = games.FindOne(context.TODO(), bson.D{{"_id", user.RecentGame}}).Decode(&game)
+		grid := game.Grid
 		if &mo.Move == nil {
 			return c.JSON(user.RecentGame)
 		}
 		if grid[int(mo.Move)] != ' ' {
-			return fiber.ErrConflict
+			return c.JSON(fiber.Map{"status": "ERROR"})
 		}
 		grid[mo.Move] = 'X'
-		playGame(&user.RecentGame)
-		if user.RecentGame.Winner != ' '
+		playGame(&game)
+		if game.Winner != ' ' {
+			update := bson.D{{"$set", bson.D{{"recentgame", newGame(games)}}}, {"$push", bson.D{{"games", user.RecentGame}}}}
+			_, _ = registeredusers.UpdateOne(context.TODO(), bson.D{{"username", user.Username}}, update)
+			return c.JSON(game)
+		} else {
+			_, err := games.UpdateOne(context.TODO(), bson.D{{"_id", user.RecentGame}}, bson.D{{"$set", bson.D{{"grid", game.Grid}}}})
+			if err != nil {
+				return err
+			}
+			return c.JSON(game)
+		}
+	})
+
+	app.Post("/listgames", func(c *fiber.Ctx) error {
+		sess, err := store.Get(c)
+		var user RegisteredUser
+
+		fmt.Println(sess.Get("username"))
+
+		err = registeredusers.FindOne(context.TODO(), bson.D{{`username`, sess.Get("username")}}).Decode(&user)
+		if err != nil {
+			fmt.Println("herr2e")
+			return c.JSON(fiber.Map{"status": "ERROR"})
+		}
+		var game Game
+		fmt.Println(user.Games)
+		gameArray := make([]Game, len(user.Games)+1)
+		for i := 0; i < len(user.Games); i++ {
+			err := games.FindOne(context.TODO(), bson.D{{"_id", user.Games[i]}}).Decode(&game)
+			if err != nil {
+				return err
+			}
+			gameArray[i] = game
+		}
+		err = games.FindOne(context.TODO(), bson.D{{"_id", user.RecentGame}}).Decode(&game)
+		if err != nil {
+			return err
+		}
+		gameArray[len(user.Games)] = game
+		jsonGames, _ := json.Marshal(gameArray)
+		//fiber.Map{"status": "OK", "games": string(jsonGames)}
+		temp, _ := json.Marshal(fiber.Map{"status": "OK", "games": string(jsonGames)})
+		fmt.Println(string(temp))
+		return c.JSON(fiber.Map{"status": "OK", "games": string(jsonGames)})
+		//return c.JSON(fmt.Sprintf(`"status":"OK", "games" : "%s"`, string(jsonGames)))
+	})
+
+	app.Post("/getgame", func(c *fiber.Ctx) error {
+		var game Game
+		if err := c.BodyParser(game); err != nil {
+			return c.JSON(fiber.Map{"status": "ERROR"})
+		}
+		err = games.FindOne(context.TODO(), bson.D{{"_id", game.Id}}).Decode(&game)
+		if err != nil {
+			return c.JSON(fiber.Map{"status": "ERROR"})
+		}
+
+		type GetGameResponse struct {
+			Status string `json:"status"`
+			Grid   []rune `json:"grid"`
+			Winner rune   `json:"winner"`
+		}
+		//return c.JSON(fiber.Map{"status": "OK", "grid": string(game.Grid), "winner": game.Winner})
+		return c.JSON(GetGameResponse{"OK", game.Grid, game.Winner})
+		//return c.JSON(fmt.Sprintf(`"status":"OK", "grid" : "%s", "winner": '%s'`, string(game.Grid), string(game.Winner)))
+	})
+
+	app.Post("/getscore", func(c *fiber.Ctx) error {
+		sess, err := store.Get(c)
+		var user RegisteredUser
+		err = registeredusers.FindOne(context.TODO(), bson.D{{`username`, sess.Get("username")}}).Decode(&user)
+		if err != nil {
+			return c.JSON(fiber.Map{"status": "ERROR"})
+		}
+		wins := 0
+		ties := 0
+		wopr := 0
+		gs := user.Games
+		var game Game
+		for i := 0; i < len(gs); i++ {
+			err := games.FindOne(context.TODO(), bson.D{{"_id", game.Id}}).Decode(&game)
+			if err != nil {
+				return c.JSON(fiber.Map{"status": "ERROR"})
+			}
+			if game.Winner == 'X' {
+				wins++
+				continue
+			} else if game.Winner == 'T' {
+				ties++
+				continue
+			} else if game.Winner == 'O' {
+				wopr++
+				continue
+			}
+		}
+
+		type GetScoreResponse struct {
+			Status string `json:"status"`
+			Human  int    `json:"human"`
+			Wopr   int    `json:"wopr"`
+			Tie    int    `json:"tie"`
+		}
+		return c.JSON(GetScoreResponse{"OK", wins, wopr, ties})
+		//return c.JSON(fmt.Sprintf(`"status":"OK", "human" : %d, "wopr": %d, "tie" : %d`, wins, wopr, ties))
 	})
 
 	FiberErr := app.Listen(":80")
